422ee822a81f4b948b5dd1f5f9d1c94bd9fe24ce
Arnaud PHELIPOT <aphelipot@gmail.com>
2017-02-15 16:50:44 +0100

    Update graphics and back end events.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var logger = require('morgan');
-var serialport = require('serialport');
 var socket_io = require('socket.io');
 
 // Instantiate App, server and websockets
@@ -37,43 +36,24 @@ app.use('/vote', vote);
 
 // Server starting and port listening
 
+var GestureDriver = require('./lib/gesture-driver');
+var gestureController = new GestureDriver();
+
+
+
 httpServer.listen(app.get('port'), function () {
+  gestureController.autoDetect();
   console.log('Example app listening on port ' + app.get('port') + ' !');
 });
 
-// COM port configuration
-
-var gesturePortNumber = undefined;
-var gestureBaudRate = 115200;
-var gesturePort;
-
-
-
-serialport.list(function (err, ports) {
-  ports.forEach(function (port) {
-    console.log({ "port": port.comName, "VendorId_ProductId": port.pnpId, "manufacturer": port.manufacturer });
-    if (port.pnpId === "FTDIBUS\\VID_0403+PID_6001+ANZ1S8W9A\\0000") {
-      console.log("Port found!");
-      gesturePortNumber = port.comName;
-      gesturePort = new serialport(gesturePortNumber, { baudRate: gestureBaudRate });
-      gesturePort.on('open', function () { console.log(gesturePortNumber + " is opened @" + gestureBaudRate + " bauds") })
-      gesturePort.on('data', function (data) {
-        console.log('Data: ' + data);
-
-      });
-    }
-    else {
-      console.log("This port is not compatible with the gesture vote application!");
-    }
-  });
-});
-
-
-
-
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  gestureController._eventEmitter.on('overOn', function (param) {
+    socket.emit('overOn', param);
+  });
+  gestureController._eventEmitter.on('overOff', function (param) {
+    socket.emit('overOff', param);
+  });
+  gestureController._eventEmitter.on('click', function (param) {
+    socket.emit('click', param);
   });
 });
